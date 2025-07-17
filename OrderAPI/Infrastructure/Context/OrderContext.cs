54865aa57f86284009e0ce90c4b8d4da8cb25185
OmarAyman1234 <omarayman9124@gmail.com>
2025-07-17 14:23:23 +0300

    Adding products table

--- OrderAPI/Infrastructure/Context/OrderContext.cs
+++ OrderAPI/Infrastructure/Context/OrderContext.cs
@@ -6,10 +6,17 @@ namespace OrderAPI.Infrastructure.Context
     public class OrderContext : DbContext
     {
         public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Data Source=OMARSLAPTOP\SQLEXPRESS; Initial Catalog=Trade;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.AddDate)
+                .HasDefaultValueSql("GETDATE()");
         }
     }
 } 
