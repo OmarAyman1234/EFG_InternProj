3fd32b5d756030f214235aaa3caf3fc5c074d6dd
OmarAyman1234 <omarayman9124@gmail.com>
2025-07-18 19:36:16 +0300

    Removed orders caching

--- OrderAPI/Application/UseCases/OrderUseCase/GetAllOrdersUseCase.cs
+++ OrderAPI/Application/UseCases/OrderUseCase/GetAllOrdersUseCase.cs
@@ -7,21 +7,13 @@ namespace OrderAPI.Application.UseCases.OrderUseCase
     public class GetAllOrdersUseCase
     {
         private readonly IOrderRepository _orderRepository;
-        private readonly ICachingService _cache;
-        public GetAllOrdersUseCase(IOrderRepository orderRepository, ICachingService cache)
+        public GetAllOrdersUseCase(IOrderRepository orderRepository)
         {
             _orderRepository = orderRepository;
-            _cache = cache;
         }
 
         public async Task<IEnumerable<OrderDto>> ExecuteAsync()
         {
-            string cacheKey = "AllOrders";
-            var cachedOrders = await _cache.GetStringAsync(cacheKey);
-            if (cachedOrders != null)
-            {
-                return JsonSerializer.Deserialize<IEnumerable<OrderDto>>(cachedOrders)!;
-            }
 
             var orders = (await _orderRepository.GetAllAsync()).Select(o => new OrderDto
             {
@@ -32,12 +24,6 @@ namespace OrderAPI.Application.UseCases.OrderUseCase
                 Quantity = o.Quantity,
                 Direction = o.Direction
             }).ToList();
-
-            await _cache.SetStringAsync(
-                cacheKey,
-                JsonSerializer.Serialize(orders),
-                TimeSpan.FromHours(1)
-            );
 
             return orders;
         }
