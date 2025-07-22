9cbe718283bcae82b59423feb70f33f349f8e242
OmarAyman1234 <omarayman9124@gmail.com>
2025-07-22 19:23:40 +0300

    AddProduct at the controller

--- OrderAPI/Application/UseCases/ProductUseCase/ProductService.cs
+++ OrderAPI/Application/UseCases/ProductUseCase/ProductService.cs
@@ -1,16 +1,20 @@
 ﻿using OrderAPI.Domain;
+using OrderSharedContent;
 
 namespace OrderAPI.Application.UseCases.ProductUseCase
 {
     public class ProductService
     {
         private readonly GetAllProductsUseCase _getAllProducts;
+        private readonly AddProductUseCase _addProductUseCase;
 
-        public ProductService(GetAllProductsUseCase getAllProducts)
+        public ProductService(GetAllProductsUseCase getAllProducts, AddProductUseCase addProductUseCase)
         {
             _getAllProducts = getAllProducts;
+            _addProductUseCase = addProductUseCase;
         }
 
         public Task<IEnumerable<Product>> GetAllAsync() => _getAllProducts.ExecuteAsync();
+        public Task<Product> AddProduct(AddProductRequest p) => _addProductUseCase.ExecuteAsync(p);
     }
 }
